859457429ca5659caf45a252eaf0864879bcb933
edggy <edgchess@aol.com>
2015-10-28 16:33:30 -0400

    Update ATMproxy.cpp

--- ATMproxy.cpp
+++ ATMproxy.cpp
@@ -1,7 +1,16 @@
 
 
+#include <iostream>           // For cerr and cout
+#include <cstdlib>            // For atoi()
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
-  exit(1);
+  if ((argc < 2) || (argc > 2)) {
+    cerr << "Usage: " << argv[0] << " <Listen Port> <Server Port>" << endl;
+    exit(1);
+  }
+  
+  return 0;
 }
 
