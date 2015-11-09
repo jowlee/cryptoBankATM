6b5d13caff66e3bd3e7cd82d21900fb9273fef17
edgchess <edgchess@aol.com>
2015-11-09 18:59:08 -0500

    Created outline and the ioThread function

--- ATMserver.cpp
+++ ATMserver.cpp
@@ -1,8 +1,82 @@
 
-#include <iostream>           // For cerr and cout
+#include "PracticalSocket.h"	// For Socket, ServerSocket, and SocketException
+#include <iostream>				// For cerr and cout
+#include <string>				// For Strings
+#include <pthread.h>			// For multithreading
 
+//HashMap of usernames to a tuple of user info or user object containing {username, pin, balance}
 
+void deposit(const String* user, unsigned long amount);
+void balance(const String* user);
+
+void *ioThread(void *threadid);
+void *socketThread(void *threadid);
 
 int main(int argc, char *argv[]) {
-  return 0;
+	if (argc != 2) {                     // Test for correct number of arguments
+		cerr << "Usage: " << argv[0] << " <Server Port>" << endl;
+		exit(1);
+	}
+	
+	//create 2 threads, one for I/O another for incomming connections
+	return 0;
+}
+
+//
+void deposit(const String* user, unsigned long amount) {
+	
+}
+
+void balance(const String* user) {
+	
+}
+
+void *ioThread(void *threadid) {
+	// Read in commands from cin
+	for(String line; getline(cin, line);) {
+		int index = 0;
+		String command = "";
+		
+		// Read input until we read a space, then for each character add it to the command string
+		for(; line[index] != " " || index >= line.length(); index++) {
+			command += line[index];
+		}
+		
+		// Skip any number of spaces
+		for(; line[index] == " " || index >= line.length(); index++);
+		
+		String username = "";
+		
+		// Read the next chars and put into username
+		for(; line[index] != " " || index >= line.length(); index++) {
+			username += line[index];
+		}
+		//deposit ​[username] [amount] ­ Increase <username>’s ​balance by <amount>
+		if(command.compare(0,8,"deposit")) {
+			String amount = "";
+			
+			// Skip any number of spaces
+			for(; line[index] == " " || index >= line.length(); index++);
+			
+			// Read until the next space and put it in amount
+			for(; line[index] != " " || index >= line.length(); index++) {
+				amount += line[index];
+			}
+			
+			// Execute deposit command
+			deposit(&username, stol(amount, nullptr))
+		}
+		//balance​ [username] ­ Print the balance of <username>
+		else if(command.compare(0,8,"balance")) {
+			
+			// Execute balance command
+			balance(&username);
+		}
+	}
+	pthread_exit(NULL);		// Ends the thread
+}
+
+void *socketThread(void *threadid){
+	
+	pthread_exit(NULL);		// Ends the thread
 }
