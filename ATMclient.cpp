303b19b259dbf044c8239d867fd068f13e53e566
Andrew Kaiser <kaisea@rpi.edu>
2015-11-09 19:36:39 -0500

    client&server socket

--- ATMclient.cpp
+++ ATMclient.cpp
@@ -1,23 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-#include "PracticalSocket.h"  // For Socket, ServerSocket, and SocketException
-#include <iostream>
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
-const unsigned int RCVBUFSIZE = 64;    // Size of receive buffer
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-
-// use 2 threads
-//  one UDPSocket thread
-//  one terminal input thread
-
-
-int main(int argc, char *argv[]) {
-  if ((argc < 3) || (argc > 4)) {     // Test for correct number of arguments
-    cerr << "Usage: " << argv[0]
-         << " <Server> <Echo String> [<Server Port>]" << endl;
-    exit(1);
-  }
-
-  TCPServerSocket proxy_sock(listen_port);
-
-  return 0;
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
 }
--- ATMserver.cpp
+++ ATMserver.cpp
@@ -1,8 +1,87 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <string>
+#include <cstring>
 
-#include <iostream>           // For cerr and cout
+void error(const char *msg)
+{
+    std::cerr << msg <<std::endl;
+    exit(1);
+}
 
+int recvMessage(int newsockfd, char  buffer[256], int length) {
+  int n;
+  n = read(newsockfd,buffer,length);
+  if (n < 0) {
+    error("ERROR reading from socket");
+  }
+  else if (n == 0) {
+    // no message
+  }
+  else {
+    std::cout << "Here is the message: " << buffer << std::endl;
+  }
+}
+int sendMessage(int newsockfd, std::string message) {
+  int n;
+  int length  = message.length();
+  const char *cstr = message.c_str();
 
+  n = write(newsockfd, cstr, length);
+  if (n < 0) {
+    error("ERROR writing to socket");
+  }
+}
 
-int main(int argc, char *argv[]) {
-  return 0;
+int main(int argc, char *argv[])
+{
+    // socket initialization
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        std::cerr << "Usage: ATMserver <Port>" << std::endl;
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("ERROR opening socket");
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    // assign socket to port number
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+        error("ERROR on binding");
+    }
+    // listens for client connections
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    // blocking wait for the connection
+    newsockfd = accept(sockfd,
+               (struct sockaddr *) &cli_addr,
+               &clilen);
+    if (newsockfd < 0) {
+        error("ERROR on accept");
+    }
+
+    bzero(buffer,256);
+    recvMessage(newsockfd, buffer, 255);
+    sendMessage(newsockfd, "I got your message");
+
+    close(newsockfd);
+    close(sockfd);
+    return 0;
 }
