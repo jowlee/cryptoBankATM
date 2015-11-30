5cac9b51b8f1035c068eb4310f8dd7a124f05176
edgchess <edgchess@aol.com>
2015-11-29 23:32:47 -0500

    bug fixes

--- ATMclient.cpp
+++ ATMclient.cpp
@@ -48,11 +48,11 @@ int main(int argc, char *argv[])
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+    n = cwrite(sockfd,buffer,strlen(buffer));
     if (n < 0)
          error("ERROR writing to socket");
     bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+    n = cread(sockfd,buffer,255);
     if (n < 0)
          error("ERROR reading from socket");
     printf("%s\n",buffer);
