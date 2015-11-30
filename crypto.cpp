578aee2304c2636e722f7713d41ddf1c8945b815
edgchess <edgchess@aol.com>
2015-11-29 22:22:08 -0500

    Getting sha256 to work

--- crypto.cpp
+++ crypto.cpp
@@ -11,7 +11,7 @@
 #include <fstream>
 #include <string>
 #include <cstring>
-#include "cryptopp/sha.h"
+#include "sha256.h"
 
 const int PACKET_DATA_LENGTH = 32;    //bytes
 const int PACKET_CHECKSUM_LENGTH = 32;
