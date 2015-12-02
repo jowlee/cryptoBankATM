e70de7c4cf779f681e3a0e946c4a2797aa46e8a3
Ethan Gertler <edgchess@aol.com>
2015-12-02 05:25:19 +0000

    Removed heap access

--- crypto.cpp
+++ crypto.cpp
@@ -18,14 +18,14 @@ const int PACKET_DATA_LENGTH = 32;    //bytes
 const int PACKET_CHECKSUM_LENGTH = 32;
 const int PACKET_LENGTH = PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH;
 
-char* OTP(unsigned long long* index, const unsigned long long amount);
-char* xorCharArray(const char* first, const char* second, unsigned long long length);
+char* OTP(char* output, unsigned long long* index, const unsigned long long amount);
+char* xorCharArray(char* output, const char* first, const char* second, unsigned long long length);
 unsigned long long unsignedLongLongRand();
-char* longToCharArray(unsigned long long num, int size);
-unsigned long long charArrayToLong(const char* data, int size);
-bool charArrayEquals(const char* data1, const char* data2, int size);
-char* concat(const char* left, const char* right, int sizel, int sizer);
-char* sha_256(char* buf);
+char* longToCharArray(char* output, unsigned long long num, unsigned long long size);
+unsigned long long charArrayToLong(const char* data, unsigned long long size);
+bool charArrayEquals(const char* data1, const char* data2, unsigned long long size);
+char* concat(char* output, const char* left, const char* right, unsigned long long sizel, unsigned long long sizer);
+char* sha_256(char* output, const char* input, unsigned long long size);
 
 
 // send command
@@ -38,19 +38,22 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 	unsigned long long* indexOfPad = new unsigned long long;
 	*indexOfPad = unsignedLongLongRand();
 	//char* nonce = longToCharArray(&indexOfPad, PACKET_LENGTH);
-	char* nonce = longToCharArray(*indexOfPad, PACKET_LENGTH);
+	char nonce[PACKET_LENGTH];
+	longToCharArray(nonce, *indexOfPad, PACKET_LENGTH);
 	//send nonce to reciever
 	int n;
 	n = write(fd, nonce, PACKET_LENGTH);
-	delete nonce;
 	if (n < 0) {
 		return -1;
 	}
 	//1
 	//char* myChecksum = xor(OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH), OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH));
-	char* p1 = OTP(indexOfPad,PACKET_LENGTH);
-	char* p2 = OTP(indexOfPad,PACKET_LENGTH);
-	char* myChecksum = xorCharArray(p1, p2, PACKET_LENGTH);
+	char p1[PACKET_LENGTH];
+	char p2[PACKET_LENGTH];
+	OTP(p1, indexOfPad, PACKET_LENGTH);
+	OTP(p2, indexOfPad, PACKET_LENGTH);
+	char checksum[PACKET_LENGTH];
+	xorCharArray(checksum, p1, p2, PACKET_LENGTH);
 	//2
 	//byte* response = response from receiver
 	char buffer[PACKET_LENGTH];
@@ -71,7 +74,7 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		}
 		return -1;
 	}
-	else if(!charArrayEquals(buffer, myChecksum, PACKET_LENGTH)) {
+	else if(!charArrayEquals(buffer, checksum, PACKET_LENGTH)) {
 		//myChecksum != response
 		return -1;
 	}
@@ -99,7 +102,8 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		#endif
 		
 		//char* key = OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH);
-		char* key = OTP(indexOfPad, PACKET_LENGTH);
+		char key[PACKET_LENGTH];
+		OTP(key, indexOfPad, PACKET_LENGTH);
 		#ifdef _DEBUG
 		std::cout << "key: ";
 		for(int i = 0; i < PACKET_LENGTH; i++) std::cout << key[i];
@@ -107,8 +111,10 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		#endif
 		
 		//char* mac = sha-256(concat(toSend, key));
-		char* mac = sha_256(concat(toSend, key, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH));
-		delete[] key;
+		char mac[PACKET_LENGTH];
+		char tmp[PACKET_LENGTH];
+		sha_256(mac, concat(tmp, toSend, key, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH), PACKET_LENGTH);
+		
 		#ifdef _DEBUG
 		std::cout << "mac: ";
 		for(int i = 0; i < PACKET_CHECKSUM_LENGTH; i++) std::cout << mac[i];
@@ -116,9 +122,9 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		#endif
 		
 		//char* message = concat(toSend, mac);
-		char* message = concat(toSend, mac, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH);
+		char message[PACKET_LENGTH];
+		concat(message, toSend, mac, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH);
 		
-		//delete[] mac;
 		#ifdef _DEBUG
 		std::cout << "message: ";
 		for(int i = 0; i < PACKET_LENGTH; i++) std::cout << message[i];
@@ -126,7 +132,8 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		#endif
 		
 		//char* pad = OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH);
-		char* pad = OTP(indexOfPad, PACKET_LENGTH);
+		char pad[PACKET_LENGTH];
+		OTP(pad, indexOfPad, PACKET_LENGTH);
 		
 		#ifdef _DEBUG
 		std::cout << "pad: ";
@@ -138,11 +145,11 @@ ssize_t cwrite(int fd, const void *buf, size_t len) {
 		#ifdef _DEBUG
 		std::cout << "Sending encrypted message" << std::endl;
 		#endif
-		char* enc = xorCharArray(message, pad, PACKET_LENGTH);
-		delete[] message;
-		delete[] pad;
+		char enc[PACKET_LENGTH];
+		xorCharArray(enc, message, pad, PACKET_LENGTH);
+		
 		n = write(fd, enc, PACKET_LENGTH);
-		delete[] enc;
+		
 		if (n < 0) {
 			return -1;
 		}
@@ -210,7 +217,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 	std::cout << "indexOfPad: " << indexOfPad << std::endl;
 	#endif
 	
-	char* message = OTP(&indexOfPad, PACKET_LENGTH);
+	char message[PACKET_LENGTH];
+	OTP(message, &indexOfPad, PACKET_LENGTH);
 	
 	#ifdef _DEBUG
 	std::cout << "message: ";
@@ -219,7 +227,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 	#endif
 	
 	//byte* pad = OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH);
-	char* pad = OTP(&indexOfPad, PACKET_LENGTH);
+	char pad[PACKET_LENGTH];
+	OTP(pad, &indexOfPad, PACKET_LENGTH);
 	
 	#ifdef _DEBUG
 	std::cout << "pad: ";
@@ -228,7 +237,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 	#endif
 	
 	//byte* reply = xor(message, pad);
-	char* reply = xorCharArray(message, pad, PACKET_LENGTH);
+	char reply[PACKET_LENGTH];
+	xorCharArray(reply, message, pad, PACKET_LENGTH);
 	
 	#ifdef _DEBUG
 	std::cout << "reply: ";
@@ -305,7 +315,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 		std::cout << "Got data" << std::endl;
 		#endif
 		//byte* key = OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH);
-		char* key = OTP(&indexOfPad, PACKET_LENGTH);
+		char key[PACKET_LENGTH];
+		OTP(key, &indexOfPad, PACKET_LENGTH);
 		
 		#ifdef _DEBUG
 		std::cout << "key: ";
@@ -314,7 +325,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 		#endif
 	
 		//pad = OTP(&indexOfPad, PACKET_DATA_LENGTH + PACKET_CHECKSUM_LENGTH);
-		char* pad = OTP(&indexOfPad, PACKET_LENGTH);
+		char pad[PACKET_LENGTH];
+		OTP(pad, &indexOfPad, PACKET_LENGTH);
 		
 		#ifdef _DEBUG
 		std::cout << "pad: ";
@@ -323,7 +335,8 @@ ssize_t cread(int fd, void *buf, size_t len) {
 		#endif
 		
 		//byte* messsage = xor(data, pad);
-		char* message = xorCharArray(data, pad, PACKET_LENGTH);
+		char message[PACKET_LENGTH];
+		xorCharArray(message, data, pad, PACKET_LENGTH);
 		
 		#ifdef _DEBUG
 		std::cout << "message: ";
@@ -340,7 +353,9 @@ ssize_t cread(int fd, void *buf, size_t len) {
 		std::cout << std::endl;
 		#endif
 
-		char* mac = sha_256(concat(message, key, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH));
+		char mac[PACKET_LENGTH];
+		char tmp[PACKET_LENGTH];
+		sha_256(mac, concat(tmp, message, key, PACKET_DATA_LENGTH, PACKET_CHECKSUM_LENGTH), PACKET_LENGTH);
 		
 		#ifdef _DEBUG
 		std::cout << "mac: ";
@@ -354,7 +369,6 @@ ssize_t cread(int fd, void *buf, size_t len) {
 			#ifdef _DEBUG
 			std::cout << "Invalid checksum" << std::endl;
 			#endif
-			delete[] mac;
 			return -1;
 		}
 		n = write(fd, "OK", 2);
@@ -385,17 +399,17 @@ ssize_t cread(int fd, void *buf, size_t len) {
 	return amount_recv;	
 }
 
-char* OTP(unsigned long long* index, const unsigned long long amount) {
+char* OTP(char* output, unsigned long long* index, const unsigned long long amount) {
 	std::ifstream pad("otp.key", std::ifstream::ate | std::ifstream::binary);
 	//std::ifstream pad;
 	//pad.open("otp.key");
 	pad.seekg(0,std::ios::end);
 	pad.seekg(*index % pad.tellg());
-	char* result = new char[amount];
-	pad.read(result, amount);
+	//char* result = new char[amount];
+	pad.read(output, amount);
 	pad.close();
 	*index += amount;
-	return result;
+	return output;
 	//byte pad[amount];  allocate amount bytes to return
 	//seek to index in OTP
 	//for(long i = 0; i < amount; i++)
@@ -404,14 +418,14 @@ char* OTP(unsigned long long* index, const unsigned long long amount) {
 	//return pad;
 }
 
-char* xorCharArray(const char* first, const char* second, unsigned long long length) {
+char* xorCharArray(char* output, const char* first, const char* second, unsigned long long size) {
 	//byte result[length];
-	char* result = new char[length];
+	//char* result = new char[length];
 	//for(long i = 0; i < length; i++) {
-	for(unsigned long long i = 0; i < length; i++) {
-		result[i] = first[i] ^ second[i];
+	for(unsigned long long i = 0; i < size; i++) {
+		output[i] = first[i] ^ second[i];
 	}
-	return result;
+	return output;
 }
 
 unsigned long long unsignedLongLongRand() {
@@ -422,16 +436,16 @@ unsigned long long unsignedLongLongRand() {
     return randULL;
 }
 
-char* longToCharArray(unsigned long long num, int size) {
-	char* result = new char[size];
+char* longToCharArray(char* output, unsigned long long num, unsigned long long size) {
+	//char* result = new char[size];
 	for(int i = 0; i < sizeof(unsigned long long); i++){
-		result[i] = num % (int)pow (2,(sizeof(char) * 8));
+		output[i] = num % (int)pow (2,(sizeof(char) * 8));
 		num /= (int)pow(2,(sizeof(char) * 8));
 	}
-	return result;
+	return output;
 }
 
-unsigned long long charArrayToLong(const char* data, int size){
+unsigned long long charArrayToLong(const char* data, unsigned long long size){
 	unsigned long long result = 0;
 	for(int i = 0; i < size; i++){
 		result += ((unsigned long long)(unsigned char)data[i]) * (unsigned long long)pow(2,i*(sizeof(char) * 8));
@@ -439,26 +453,25 @@ unsigned long long charArrayToLong(const char* data, int size){
 	return result;
 }
 
-bool charArrayEquals(const char* data1, const char* data2, int size) {
+bool charArrayEquals(const char* data1, const char* data2, unsigned long long size) {
 	for(int i = 0; i < size; i++) {
 		if(data1[i] != data2[i]) return false;
 	}
 	return true;
 }
 
-char* concat(const char* left, const char* right, int sizel, int sizer) {
-	char* result = new char[sizel + sizer];
+char* concat(char* output, const char* left, const char* right, unsigned long long sizel, unsigned long long sizer) {
 	for(int i = 0; i < sizel; i++) {
-		result[i] = left[i];
+		output[i] = left[i];
 	}
 	for(int i = 0; i < sizer; i++) {
-		result[sizel + i] = right[i];
+		output[sizel + i] = right[i];
 	}
-	return result;
+	return output;
 }
 
-char* sha_256(char* buf) {
-	return &sha256(buf)[0];
+char* sha_256(char* output, const char* input, unsigned long long size) {
+	return sha256(output, input, size);
 }
 
 //For testing     
